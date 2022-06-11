5f62a92a4d6a1ed2f60844d52ad6fde97c72010d
Felix Titov <titow.feliks@yandex.ru>
2022-06-11 16:56:05 +0300

    Created custom CollectionViewCell

---
+++ PhotoCollectionView/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoCollectionView
+//
+//  Created by Felix Titov on 6/11/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
